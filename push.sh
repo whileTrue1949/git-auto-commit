268c86c153ada6aada9a461f0115023a6dc212a7
wuzhixu <wuzhixu@do-global.com>
2021-03-27 21:48:55 +0800

    add random func && rm loop.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,13 @@
-git pull && /usr/local/bin/node add.js && git commit -a -m 'git auto commit' && git push origin master
+function rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(($RANDOM+1000000000)) #增加一个10位的数再求余
+    echo $(($num%$max+$min))
+}
+rnd=$(rand 2 10)
+for i in $(seq 1 rnd)
+do
+    git pull && /usr/local/bin/node add.js && git commit -a -m 'git auto commit' && git push origin master
+done
 
 
